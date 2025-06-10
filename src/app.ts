30f8e177eb4383308f92f43f3ccd8dce9d9b01ae
unknown <redanom@mail.ru>
2025-06-10 17:33:57 +0500

    big append

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import routes from './routes'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use('/static', express.static(path.join(process.cwd(), 'public')))
 app.use(routes)
 
 app.get('/api/ping', (_req, res) => {
